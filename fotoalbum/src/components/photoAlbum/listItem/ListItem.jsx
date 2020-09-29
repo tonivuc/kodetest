3988225fffda55032ad0e0bbf4002bd761d2a367
Toni Vucic <tonivuc@gmail.com>
2020-09-29 20:51:46 +0200

    Styling and cleanup

--- fotoalbum/src/components/photoAlbum/listItem/ListItem.jsx
+++ fotoalbum/src/components/photoAlbum/listItem/ListItem.jsx
@@ -3,10 +3,6 @@ import { Segment } from 'semantic-ui-react';
 import './ListItem.css';
 
 export default class ListItem extends Component {
-  
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return (
